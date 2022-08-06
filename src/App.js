30f99f56e1ec55de56d5067be084a2cecaa2d30e
anhdev2k1 <info.anh2001@gmail.com>
2022-08-06 20:23:28 +0700

    readme

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="trellow-master">
+      <nav className="navbar app">App bar</nav>
+      <nav className="navbar board">Board bar</nav>
+      <nav className="board-colums">board colums</nav>
+    </div>
+  );
+}
+
+export default App;
